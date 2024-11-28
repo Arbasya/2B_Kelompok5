b610f0e0940da91e46cc7df72366f39adf10188b
Arbasya <arbasyaa@gmail.com>
2024-11-28 09:22:35 +0700

    commit update

--- routes.php
+++ routes.php
@@ -1,27 +1,27 @@
 <?php
-// Accommodations_routes.php
+// routes.php
 
-require_once 'app/controllers/user5controllers.php';
+require_once 'app/controllers/ReservationController.php';
 
-$controller = new User5Controller();
+$controller = new ReservationController();
 $url = $_SERVER['REQUEST_URI'];
 $requestMethod = $_SERVER['REQUEST_METHOD'];
 
-if ($url == '/user5/index' || $url == '/') {
+if ($url == '/Reservation/index' || $url == '/') {
     $controller->index();
-} elseif ($url == '/user5/create' && $requestMethod == 'GET') {
+} elseif ($url == '/Reservation/create' && $requestMethod == 'GET') {
     $controller->create();
-} elseif ($url == '/user5/store' && $requestMethod == 'POST') {
+} elseif ($url == '/Reservation/store' && $requestMethod == 'POST') {
     $controller->store();
-} elseif (preg_match('/\/user5\/edit\/(\d+)/', $url, $matches) && $requestMethod == 'GET') {
-    $user5Id = $matches[1];
-    $controller->edit($user5Id);
-} elseif (preg_match('/\/user5\/update\/(\d+)/', $url, $matches) && $requestMethod == 'POST') {
-    $user5Id = $matches[1];
-    $controller->update($user5Id, $_POST);
-} elseif (preg_match('/\/user5\/delete\/(\d+)/', $url, $matches) && $requestMethod == 'GET') {
-    $user5Id = $matches[1];
-    $controller->delete($user5Id);
+} elseif (preg_match('/\/Reservation\/edit\/(\d+)/', $url, $matches) && $requestMethod == 'GET') {
+    $userId = $matches[1];
+    $controller->edit($userId);
+} elseif (preg_match('/\/Reservation\/update\/(\d+)/', $url, $matches) && $requestMethod == 'POST') {
+    $userId = $matches[1];
+    $controller->update($userId, $_POST);
+} elseif (preg_match('/\/Reservation\/delete\/(\d+)/', $url, $matches) && $requestMethod == 'GET') {
+    $userId = $matches[1];
+    $controller->delete($userId);
 } else {
     http_response_code(404);
     echo "404 Not Found";
