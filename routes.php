2559f171896e7c39d1d8122f8f6e44254b29ca7b
Arbasya <arbasyaa@gmail.com>
2024-11-28 10:08:01 +0700

    routes update

--- routes.php
+++ routes.php
@@ -4,19 +4,13 @@
 require_once 'app/controllers/user5controllers.php';
 require_once 'app/controllers/AccommodationsController.php';
 require_once 'app/controllers/user5controllers.php';
-require_once 'app/controllers/ReservationController.php';
-require_once 'app/controllers/AccommodationsController.php';
 
 $controller = new AccommodationsController();
 $controller = new User5Controller();
 $controller = new ReservationController();
-$controller = new User5Controller();
-$controller = new AccommodationsController();
 $url = $_SERVER['REQUEST_URI'];
 $requestMethod = $_SERVER['REQUEST_METHOD'];
 
-if ($url == '/Accommodations/index' || $url == '/') {
-if ($url == '/user5/index' || $url == '/') {
 if ($url == '/Accommodations/index' || $url == '/') {
 if ($url == '/user5/index' || $url == '/') {
 if ($url == '/Reservation/index' || $url == '/') {
@@ -24,11 +18,7 @@ if ($url == '/Reservation/index' || $url == '/') {
 } elseif ($url == '/user5/create' && $requestMethod == 'GET') {
 } elseif ($url == '/Reservation/create' && $requestMethod == 'GET') {
 } elseif ($url == '/Accommodations/create' && $requestMethod == 'GET') {
-} elseif ($url == '/user5/create' && $requestMethod == 'GET') {
-} elseif ($url == '/Accommodations/create' && $requestMethod == 'GET') {
     $controller->create();
-} elseif ($url == '/user5/store' && $requestMethod == 'POST') {
-} elseif ($url == '/Accommodations/store' && $requestMethod == 'POST') {
 } elseif ($url == '/user5/store' && $requestMethod == 'POST') {
 } elseif ($url == '/Reservation/store' && $requestMethod == 'POST') {
 } elseif ($url == '/Accommodations/store' && $requestMethod == 'POST') {
@@ -60,15 +50,6 @@ if ($url == '/Reservation/index' || $url == '/') {
 } elseif (preg_match('/\/Reservation\/delete\/(\d+)/', $url, $matches) && $requestMethod == 'GET') {
     $userId = $matches[1];
     $controller->delete($userId);
-} elseif (preg_match('/\/user5\/edit\/(\d+)/', $url, $matches) && $requestMethod == 'GET') {
-    $user5Id = $matches[1];
-    $controller->edit($user5Id);
-} elseif (preg_match('/\/user5\/update\/(\d+)/', $url, $matches) && $requestMethod == 'POST') {
-    $user5Id = $matches[1];
-    $controller->update($user5Id, $_POST);
-} elseif (preg_match('/\/user5\/delete\/(\d+)/', $url, $matches) && $requestMethod == 'GET') {
-    $user5Id = $matches[1];
-    $controller->delete($user5Id);
 } else {
     http_response_code(404);
     echo "404 Not Found";
